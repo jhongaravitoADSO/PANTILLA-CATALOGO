c4d9ace102724d4542e8d1ddd0393f6643e227e5
jhon jairo garavito <149621084+jhongaravitoADSO@users.noreply.github.com>
2025-01-02 16:12:21 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 const products = [
     { id: 1, name: "Cortadora de cabello", price: "$75.000", description: "Cortadora de pelo eléctrica HIENA,cortadora de acabado de peluquero,cortadora de barba inalambrica recargable por USB, HYN-212 de corte de pelo húmedo y seco.",image: "imagenes/maquina de cortar.jpeg" },
     { id: 2, name: "Hojas de afeitar ultra", price: "$2.000", description: "Hojas Caja 5 unidades a 2000 unidad 500", image: "imagenes/hoja de afeitar.jpeg" },
-    { id: 3, name: "Maquina de afeitar inoxidable", price: "$29.000", description: "Maquina de afeitar inoxidable de hojas cambiables con estuche espejo.", image: "imagenes/maquina de afeitar.jpeg" },
+    { id: 3, name: "Maquina de afeitar inoxidable", price: "$35.000", description: "Maquina de afeitar inoxidable de hojas cambiables con estuche espejo.", image: "imagenes/maquina de afeitar.jpeg" },
     { id: 4, name: "Bomba de aire manual", price: "$55.000", description: "Bomba de aire manual para inflador de neumáticos, bicicletas de montaña, válvula scharader, accesoríos,80 PSI.", image: "imagenes/bomba.jpeg" },
     { id: 5, name: "Y80 ", price: "$50.000", description: "Y80 con luz ambiental digital, WTS Bluetooth 5,deportivos,durabilidad de la carga y almacena carga la cajas.", image: "imagenes/y80.jpeg" },
     { id: 6, name: "Cargador rápido de 120w", price: "$40.000", description: "Cargador rápido de 120w tipo C USB C PD adaptador de cargador USB 3,0 de carga rápida.", image: "imagenes/cargador.jpeg" },
